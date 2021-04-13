e784440638bcea02a17fbcfdf26ec32e4ffe958f
Ethan Weinberger <ewein@cs.washington.edu>
2021-04-12 21:48:54 -0700

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+    name="hd_ae",
+    version="0.0.1",
+    author="Ethan Weinberger",
+    author_email="ewein@cs.washington.edu",
+    description="For producing pretrained embedding models of scRNA-seq data.",
+    long_description="""
+        HD-AE (Hilbert-Schmidt Deconfounded Autoencoder is a deep learning model
+        traine to produce integrated embedidngs of scRNA-seq data. Pretrained HD-AE
+        models can be used to embed new batches of data at test time, without needing
+        to retrain the model.
+    """,
+    long_description_content_type="text/markdown",
+    url="https://github.com/suinleelab/hd_ae",
+    packages=['hd_ae'],
+    install_requires=[
+        'torch',
+        'pytorch-lightning',
+        'scanpy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering"
+    ],
+    python_requires='>=3.6',
+)
